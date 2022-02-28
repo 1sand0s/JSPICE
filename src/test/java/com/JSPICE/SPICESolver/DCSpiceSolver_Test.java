da9d483d850b4c265ac49bc1d9ae76b8ddf039e8
1sand0s <1sand0sardpi@gmail.com>
2022-02-28 02:17:50 -0600

    Adding test case for wheatstone bridge

--- src/test/java/com/JSPICE/SPICESolver/DCSpiceSolver_Test.java
+++ src/test/java/com/JSPICE/SPICESolver/DCSpiceSolver_Test.java
@@ -21,7 +21,7 @@ import static org.junit.Assert.assertTrue;
 public class DCSpiceSolver_Test {
 
     /**
-     * regular
+     * @brief Test case for a simple voltage divider
      * 
      * @author 1sand0s
      * @since 1.0.0
@@ -67,7 +67,7 @@ public class DCSpiceSolver_Test {
 	 *         ~-----^v^v^v^v^v---------~
 	 *         |                        |
 	 *         |                        |
-	 *        ~~~                       >
+	 *        ~~~                       >                                    
 	 *       ~ + ~ source               < r2
 	 *       ~ - ~   10V                > 100
 	 *        ~ ~                       <
@@ -102,7 +102,115 @@ public class DCSpiceSolver_Test {
 
 	/* Solve for unknown node voltages and branch currents */
         solver.solve();
+	
+	/* Assert if solver result matches expected solution */
+        assertTrue(ComplexMatrixOperations.compareMatrices(x, solver.getResult(), tol));
+    }
 
+    /**
+     * @brief Test case for Wheatstone bridge topology
+     * 
+     * @author 1sand0s
+     * @since 1.0.0
+     * @version 1.0.0
+     */
+    @Test
+    public void testWheatstoneBridge_DC() {
+	/* Tolerance for comparing solution */
+	double tol = 1e-5;
+
+	/* Solution after DC analysis */
+        Complex x[][] = { { new Complex(10, 0) }, { new Complex(5, 0) }, { new Complex(5, 0) }, { new Complex(-0.2, 0) } };
+
+	/* Instantiate DCSpiceSolver */
+	AbstractSpiceSolver solver = new DCSpiceSolver();
+
+	/* Create a DC Source*/
+	DCVoltage source = new DCVoltage();
+
+	/* Create resistors */
+	Resistor r1 = new Resistor();
+        Resistor r2 = new Resistor();
+	Resistor r3 = new Resistor();
+	Resistor r4 = new Resistor();
+	Resistor r5 = new Resistor();
+	
+	/* Create circuit GND element */
+	GND g1 = new GND();
+
+	/* Create wires to connect circuit elements */
+        Wire w1 = new Wire();
+        Wire w2 = new Wire();
+        Wire w3 = new Wire();
+	Wire w4 = new Wire();
+
+	/* Set DC source voltage to 10V */
+        source.setValue(10);
+
+	/* Set r1 and r2 resistances to 100 Ohm each */
+	r1.setValue(100);
+        r2.setValue(100);
+	r3.setValue(100);
+	r4.setValue(100);
+	r5.setValue(100);
+
+	/*                  Circuit Topology
+	 *
+	 *                           - w2
+	 *                          / \
+	 *                         /   \
+	 *                        R1    R2
+         *                       /       \
+	 *               w1     /         \
+	 *         ~-----------/-----R5----\-~
+	 *         |           \           / |
+	 *         |            \         /  |
+	 *        ~~~            \       /   |                                   
+	 *       ~ + ~ source     R3    R4   |
+	 *       ~ - ~   10V       \   /     |
+	 *        ~ ~               \ /      |
+	 *         |                 - w3    |
+         *         |          w4             |
+	 *         ~-------------------------~
+	 *       -----  
+	 *        --- g1
+	 *         -
+	 */
+
+	/* Use wires to connect the circuit elements as shown above */
+        w1.addTerminal(source, ComponentTerminals.POS_NODE);
+        w1.addTerminal(r1, ComponentTerminals.POS_NODE);
+	w1.addTerminal(r3, ComponentTerminals.POS_NODE);
+	w1.addTerminal(r5, ComponentTerminals.POS_NODE);
+
+        w2.addTerminal(r1, ComponentTerminals.NEG_NODE);
+        w2.addTerminal(r2, ComponentTerminals.POS_NODE);
+
+	w3.addTerminal(r3, ComponentTerminals.NEG_NODE);
+        w3.addTerminal(r4, ComponentTerminals.POS_NODE);
+
+        w4.addTerminal(r2, ComponentTerminals.NEG_NODE);
+	w4.addTerminal(r4, ComponentTerminals.NEG_NODE);
+	w4.addTerminal(r5, ComponentTerminals.NEG_NODE);
+	w4.addTerminal(source, ComponentTerminals.NEG_NODE);
+        w4.addTerminal(g1, ComponentTerminals.GND);
+
+	/* Add circuit elements to the solver */
+        solver.addElement(source);
+        solver.addElement(r1);
+        solver.addElement(r2);
+	solver.addElement(r3);
+	solver.addElement(r4);
+	solver.addElement(r5);
+	solver.addElement(g1);
+        solver.addWire(w1);
+        solver.addWire(w2);
+        solver.addWire(w3);
+	solver.addWire(w4);
+
+	/* Solve for unknown node voltages and branch currents */
+        solver.solve();
+	
 	/* Assert if solver result matches expected solution */
         assertTrue(ComplexMatrixOperations.compareMatrices(x, solver.getResult(), tol));
     }
