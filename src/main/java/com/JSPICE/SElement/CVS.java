8cd559cb435080e0bef13234557ffba6b0a0f9b4
1sand0s <1sand0sardpi@gmail.com>
2022-05-21 01:01:14 -0500

    Documenting code

--- src/main/java/com/JSPICE/SElement/CVS.java
+++ src/main/java/com/JSPICE/SElement/CVS.java
@@ -23,14 +23,42 @@ public abstract class CVS extends VSource {
 				  new ComponentTerminals[] { ComponentTerminals.POS_NODE, ComponentTerminals.NEG_NODE });
     }
 
+    /**
+     * @brief Sets the voltage gain of the dependent voltage source
+     * 
+     * @author 1sand0s
+     * @param double : gain The gain of the dependent voltage source
+     * @since 1.0.0
+     * @version 1.0.0
+     */
     public void setGain(double gain){
 	this.gain = gain;
     }
 
+    /**
+     * @brief Sets the positive reference node for the dependent source
+     * 
+     * @author 1sand0s
+     * @param Wire : dependentPositiveRef The circuit node corresponding 
+     *                                    to the positive ref of the 
+     *                                    dependent source
+     * @since 1.0.0
+     * @version 1.0.0
+     */
     public void setPositiveReference(Wire dependentPositiveRef){
 	this.dependentPositiveRef = dependentPositiveRef;
     }
 
+    /**
+     * @brief Sets the negative reference node for the dependent source
+     * 
+     * @author 1sand0s
+     * @param Wire : dependentNegativeRef The circuit node corresponding 
+     *                                    to the negative ref of the 
+     *                                    dependent source
+     * @since 1.0.0
+     * @version 1.0.0
+     */
     public void setNegativeReference(Wire dependentNegativeRef){
 	this.dependentNegativeRef = dependentNegativeRef;
     }
